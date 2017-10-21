8c2316f7cffc0064e92fdbfec4f4f46a4059a2b3
avka89 <32968414+avka89@users.noreply.github.com>
2017-10-21 15:58:56 +0200

    task2javascript

---
+++ js/app.js
@@ -0,0 +1,16 @@
+console.log("działa!");
+var myName = 'Agnieszka';
+console.log('myName', myName);
+var friends = ['Basia', 'Ania', 'Asia'];
+console.log(friends[1]);
+friends[0] = "Ala";
+console.log(friends);
+var a = 27;
+var b = 15;
+if (a > b) {
+console.log("hurra!");
+};
+
+for (var i=0; i<=friends.length-1;i++){
+console.log(friends[i]);
+};
